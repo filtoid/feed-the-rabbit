97c167d6c339bc97f92b2b4d932901ea102836e3
Phil Jeffes <philjeffes@hotmail.com>
2016-03-20 12:06:16 +0000

    Wait when we are at top or bottom of descent

--- rabbit.py
+++ rabbit.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 class Rabbit():
     def __init__(self, x, y):
@@ -8,17 +9,24 @@ class Rabbit():
         self.rect.x = x
         self.rect.y = y
         self.status = 'down'
+        self.wait = 0
 
     def draw(self, screen):
         screen.blit(self.img, self.rect)
 
     def update(self):
+        if self.wait != 0:
+            self.wait -= 1
+            return
+
         if self.status == 'down':
             self.rect.y += 1
         elif self.status=='up':
             self.rect.y -= 1
 
         if(self.rect.y>200):
+            self.wait = random.randint(20, 90)
             self.status = 'up'
         elif(self.rect.y < 110):
+            self.wait = random.randint(10, 90)
             self.status = 'down'
